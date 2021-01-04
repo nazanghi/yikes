d1ecd697c772a0a599d95d758a9f26c7ba650517
Nico Zanghi <nico.zang7@gmail.com>
2021-01-04 13:33:56 -0500

    Create AccountController

---
+++ controllers/AccountController.js
@@ -0,0 +1,57 @@
+const {Account} = require('../models')
+
+const CreateAccount = async (request, response) => {
+    try {
+        let accountDetails = {
+            ...request.body
+        }
+        let newAccount = await Account.create(accountDetails)
+        response.send(newAccount)
+        console.log('!** Account hits')
+    } catch (error) {
+        console.log('!** Account fails')
+        throw error
+    }
+}
+ 
+const GetOneAccount = async (request, response) => {
+    try {
+        const accountDetails = await Account.findByPk(request.params.id)
+        response.send(accountDetails)
+        console.log('GetOneAccount hits')
+    } catch (error) {
+        console.log('GetOneAccount fails')
+        throw error
+    }
+}
+ 
+const GetAllAccounts = async (request, response) => {
+    try {
+        const allAccounts = await Account.findAll()
+        response.send(allAccounts)
+        console.log('GetAllAccounts hits')
+    } catch (error) {
+        console.log('GetAllAccounts fails')
+        throw error
+    }
+}
+ 
+const GetAccountByUserId = async (request, response) => {
+    try {
+        const accountDetails = await Account.findAll({
+            where: {user_id: request.body.user_id}
+        })
+        console.log('GetOneAccountByUserId hits')
+    } catch (error) {
+        console.log('GetOneAccountByUserId fails')
+        throw error
+    }
+}
+ 
+
+module.exports = {
+    CreateAccount,
+    GetOneAccount,
+    GetAllAccounts,
+    GetAccountByUserId
+}
