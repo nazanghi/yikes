1490c350848f0047c7879f6f2e897990da8ffc4a
Nico Zanghi <nico.zang7@gmail.com>
2021-01-04 13:23:12 -0500

    Create UserController, middleware

---
+++ controllers/UserController.js
@@ -0,0 +1,68 @@
+const {hashPassword, passwordValid, createToken} = require('../middleware')
+//Need to make models
+const {User} = require('../models')
+
+const CreateUser = async (request, response) => {
+    try{
+        const {username, email, password } = request.body
+        const passwordDigest = await hashPassword(password)
+        const user = await User.create({username, email, passwordDigest})
+        console.log('CreateUser hits')
+        response.send(user)
+    }catch(error) {
+        console.log('CreateUser fails')
+        throw error
+    }
+}
+
+const SessionStatus = async (request, response) => {
+    try {
+        const { token } = response.locals
+        const user = await User.findByPk(token._id, {
+            attributes: ['id', 'username', 'email']
+        })
+    } catch (error){
+        console.log('SessionStatus fails')
+        throw error
+    }
+}
+ 
+const GetUser = async (request, response) => {
+    try {
+        const user = await User.findByPk(request.params.user_id)
+        console.log('GetUser hits')
+        response.send(user)
+    } catch (error){
+        console.log('GetUser fails')
+        throw error
+    }
+}
+ 
+const LoginUser = async (request, response) => {
+    try {
+        const user = await User.findOne({
+            where: {email:request.body.email},
+            raw: true
+        })
+        if (user && (await passwordValid(request.body.password, user.passwordDigest))) {
+            let payload = {
+                _id: user.id,
+                username: user.username
+            }
+            let token = createToken(payload)
+            return response.send({user, token})
+        }
+        return (response.status(401).send({message:`Wrong Password!`}))
+    } catch (error){
+        console.log('Login User fails')
+        throw error
+    }
+}
+
+
+module.exports = {
+    CreateUser,
+    GetUser,
+    LoginUser,
+    SessionStatus
+}
