ef9e52a6974838912646453e3d98a01c3e55759e
Nico Zanghi <nico.zang7@gmail.com>
2021-01-05 12:26:22 -0500

    create models sequelize

---
+++ models/review.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Review.init({
+    location: DataTypes.STRING,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    rating: DataTypes.INTEGER,
+    link: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,10 @@
 const Router = require("express").Router()
 const UserRouter = require('./UserRouter')
 const ReviewRouter = require('./ReviewRouter')
+const AccountRouter = reuqire("./AccountRouter")
 
 Router.use("/user", UserRouter)
 Router.use("/review", ReviewRouter)
+Router.use("/account", AccountRouter)
 
 module.exports = Router
